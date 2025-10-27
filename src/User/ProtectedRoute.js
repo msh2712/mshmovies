1df0f7b41e304c08a743d81eff883fbebe8d56b9
Mahesh <mshpatil369.com>
2025-10-27 20:19:18 +0530

    h

--- src/User/ProtectedRoute.js
+++ src/User/ProtectedRoute.js
@@ -1,20 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor } from './store';
-import Loading from "../Component/Loading";
 
 const ProtectedRoute = ({ children }) => {
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
-  const isRehydrated = persistor.getState().bootstrapped;
-
-  if (!isRehydrated) {
-    return <Loading/>
-  }
 
   if (!isAuthenticated) {
-    return <Navigate to="/" />;
+    return <Navigate to="/" replace />;
   }
 
   return children;
